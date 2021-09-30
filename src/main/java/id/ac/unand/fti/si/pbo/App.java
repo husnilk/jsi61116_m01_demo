df6bde6d9df303cc6923d618701752b61a3ae5a1
husnilk <husnil.k@gmail.com>
2021-09-30 16:20:41 +0700

    Update App.java

--- src/main/java/id/ac/unand/fti/si/pbo/App.java
+++ src/main/java/id/ac/unand/fti/si/pbo/App.java
@@ -3,7 +3,7 @@ package id.ac.unand.fti.si.pbo;
 public class App {
     
     public static void main(String[] args) {
-        System.out.println("Semangat...! Kamu pasti bisa...!");
+        System.out.println("Hello World!");
     }
     
 }
